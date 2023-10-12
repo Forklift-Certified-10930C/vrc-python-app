d772cbeb7b853ad72a4981ad35320f4a7b356047
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-10-12 14:25:03 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 # Imports
-import vex
 from vex import *
 
 # Parts
-brain = vex.Brain()
-controller = vex.Controller()
-motor_1 = vex.Motor(vex.Ports.PORT1, vex.GearSetting.RATIO_18_1, False)
-motor_2 = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
+brain = Brain()
+controller = Controller()
+motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+motor_2 = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 drivetrain = Drivetrain(motor_1,motor_2)
 
 # Variables
-
+drivercontrol = False
 
 # Functions
 def print_brain(msg):
@@ -20,6 +19,10 @@ def print_brain(msg):
 # Main
 def Main():
     print_brain('Initialized..')
+
+    while drivercontrol:
+        drivetrain.drive(FORWARD, controller.axis3.position(), PERCENT)
+        drivetrain.turn(RIGHT, controller.axis4.position(), PERCENT)
 
 # Inits
 Main()
