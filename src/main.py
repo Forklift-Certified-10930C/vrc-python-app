4b84dadd897a323f3f0bd24d9c69c565ff8d91ef
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-10-14 19:22:28 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
-# Imports
 import vex
 from vex import *
 
-# Parts
 brain = vex.Brain()
 controller = vex.Controller()
 leftmotor = vex.Motor(vex.Ports.PORT1, vex.GearSetting.RATIO_18_1, False)
 rightmotor = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
 drivetrain = vex.Drivetrain(leftmotor, rightmotor, gear_setting=vex.GEAR_RATIO_18, wheel_diameter=4.0, distance_units=vex.DIST_IN)
 
-# Variables
 drivercontrol = False
 field_pos = None
 
-# Functions
 def print_brain(msg):
     if '[RUNNING]' in msg:
         brain.screen.set_font_color(vex.colors.YELLOW)
@@ -35,7 +31,6 @@ def print_brain(msg):
         brain.screen.set_font_color(255, 255, 255)
         brain.screen.new_line()
 
-# Main
 def Main():
     print_brain('Initialized [RUNNING]')
     print_brain('Initialized [OKAY]')
@@ -67,6 +62,4 @@ def Main():
         right_stick_x = controller.axis2.position()
 
         drivetrain.drive(left_stick_y, right_stick_x)
-
-# Initialize
 Main()
