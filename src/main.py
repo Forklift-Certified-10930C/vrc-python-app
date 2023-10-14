22e1f76c0109e153930fe8e7c9cc615695a16eff
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-10-14 19:36:12 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -8,28 +8,12 @@ rightmotor = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
 drivetrain = vex.Drivetrain(leftmotor, rightmotor, gear_setting=vex.GEAR_RATIO_18, wheel_diameter=4.0, distance_units=vex.DIST_IN)
 
 drivercontrol = False
-field_pos = None
+field_position = None
 
 def print_brain(msg):
-    if '[RUNNING]' in msg:
-        brain.screen.set_font_color(vex.colors.YELLOW)
         brain.screen.print(msg)
-        brain.screen.set_font_color(255, 255, 255)
         brain.screen.new_line()
-    elif '[OKAY]' in msg:
-        brain.screen.set_font_color(vex.colors.GREEN)
-        brain.screen.print(msg)
-        brain.screen.set_font_color(255, 255, 255)
-        brain.screen.new_line()
-    elif '[FAILED]' in msg:
-        brain.screen.set_font_color(vex.colors.RED)
-        brain.screen.print(msg)
-        brain.screen.set_font_color(255, 255, 255)
-        brain.screen.new_line()
-    else:
-        brain.screen.print(msg)
-        brain.screen.set_font_color(255, 255, 255)
-        brain.screen.new_line()
+
 
 def Main():
     print_brain('Initialized [RUNNING]')
@@ -38,22 +22,22 @@ def Main():
     async def Autonomous():
         print_brain('Autonomous Routine [RUNNING]')
         try:
-            if field_pos == 1:
+            if field_position == 1:
                 pass
-            elif field_pos == 2:
+            elif field_position == 2:
                 pass
-            elif field_pos == -1:
+            elif field_position == -1:
                 pass
-            elif field_pos == -2:
+            elif field_position == -2:
                 pass
-            elif field_pos is None:
-                raise ValueError(f'Autonomous Routine [FAILED]: Because of {field_pos} value')
+            elif field_position is None:
+                raise ValueError(f'Autonomous Routine [FAILED]: Because of {field_position} value')
             else:
                 raise ValueError('Autonomous Routine [FAILED]: Because of Unknown Error')
             print_brain('Autonomous Routine [OKAY]')
         except Exception as e:
             print_brain(str(e))
-        drivercontrol = True
+    drivercontrol = True
 
     Autonomous()
 
