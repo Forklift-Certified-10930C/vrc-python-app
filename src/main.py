8809ec49699e435f1f52d69adb69e291935dbdfb
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-10-15 13:46:27 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -11,40 +11,34 @@ drivercontrol = False
 field_position = None
 
 def print_brain(msg):
-        brain.screen.print(msg)
-        brain.screen.new_line()
+    brain.screen.print(msg)
+    brain.screen.new_line()
 
 def Main():
     print_brain('Initialized [RUNNING]')
     print_brain('Initialized [OKAY]')
 
-    async def Autonomous():
-        print_brain('Autonomous Routine [RUNNING]')
-        print_brain('Autonomous Routine [RUNNING]')
-        try:
-            BLUE_RIGHT = 1
-            BLUE_LEFT = 2
-            RED_RIGHT = -1
-            RED_LEFT = -2
+async def Autonomous():
+    print_brain('Autonomous Routine [RUNNING]')
+    BLUE_RIGHT = 1
+    if field_position == BLUE_RIGHT:
+        print_brain('Autonomous Routine [OKAY]')
+        BLUE_LEFT = 2
+    elif field_position == BLUE_LEFT:
+        print_brain('Autonomous Routine [OKAY]')
+    elif field_position == RED_RIGHT:
+        print_brain('Autonomous Routine [OKAY]')
+        RED_RIGHT = -1
+    elif field_position == RED_LEFT:
+        print_brain('Autonomous Routine [OKAY]')
+        RED_LEFT = -2
+    elif field_position is None:
+        raise ValueError(f'Autonomous Routine [FAILED]: Because of {field_position} value')
+    else:
+        raise ValueError('Autonomous Routine [FAILED]: Because of Unknown Error')
 
-            switch_field_position = {
-                BLUE_RIGHT: 'Autonomous Routine [OKAY]',
-                BLUE_LEFT: 'Autonomous Routine [OKAY]',
-                RED_RIGHT: 'Autonomous Routine [OKAY]',
-                RED_LEFT: 'Autonomous Routine [OKAY]',
-            }
-
-            result = switch_field_position.get(field_position, None)
-            if result is not None:
-                print_brain(result)
-            elif field_position is None:
-                raise ValueError(f'Autonomous Routine [FAILED]: Because of {field_position} value')
-            else:
-                raise ValueError('Autonomous Routine [FAILED]: Because of Unknown Error')
-        except Exception as e:
-            print_brain(f'Autonomous Routine [FAILED]: Because {str(e)}')
-        drivercontrol = True
-
+try:
+    drivercontrol = True
     Autonomous()
 
     while drivercontrol:
@@ -52,5 +46,7 @@ def Main():
         right_stick_x = controller.axis2.position()
 
         drivetrain.drive(vex.DirectionType.FWD, left_stick_y, vex.VelocityUnits.PCT)
+except Exception as e:
+    print_brain(f'Error: {str(e)}')
 
 Main()
