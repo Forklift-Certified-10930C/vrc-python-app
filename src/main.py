33b6f1f1ee2883b1c5d9a910d8ee3e3f33a67549
vx-clutch <owestness@gmail.com>
2023-10-20 20:45:36 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -14,53 +14,54 @@ def print_brain(msg):
     brain.screen.print(msg)
     brain.screen.new_line()
 
+def Autonomous():
+    print_brain('Autonomous Routine [RUNNING]')
+
+    class Positions:
+        BLUE_RIGHT = 1
+        BLUE_LEFT = 2
+        RED_RIGHT = -1
+        RED_LEFT = -2
+
+    if Positions.BLUE_RIGHT == field_position:
+        print_brain('Autonomous Routine [OKAY]')
+
+    elif Positions.BLUE_LEFT == field_position:
+        print_brain('Autonomous Routine [OKAY]')
+
+    elif Positions.RED_RIGHT == field_position:
+        drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
+        drivetrain.turn_for(vex.TurnType.RIGHT, 100, vex.RotationUnits.DEG)
+        print_brain('Autonomous Routine [OKAY]')
+
+    elif Positions.RED_LEFT == field_position:
+        print_brain('Autonomous Routine [OKAY]')
+
+    elif None == field_position:
+        print_brain('Autonomous Routine [FAILED]: None Variable Value')
+    else:
+        print_brain('Autonomous Routine [FAILED]: Unknown Error')
+
+    drivercontrol = True
+
 def Main():
     print_brain('Initialized [RUNNING]')
     print_brain('Initialized [OKAY]')
 
-    def Autonomous():
-        print_brain('Autonomous Routine [RUNNING]')
-
-        class Positions:
-            BLUE_RIGHT = 1
-            BLUE_LEFT = 2
-            RED_RIGHT = -1
-            RED_LEFT = -2
-
-        if Positions.BLUE_RIGHT == field_position:
-            print_brain('Autonomous Routine [OKAY]')
-
-        elif Positions.BLUE_LEFT == field_position:
-            print_brain('Autonomous Routine [OKAY]')
-
-        elif Positions.RED_RIGHT == field_position:
-            drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
-            drivetrain.turn_for(vex.TurnType.RIGHT, 100, vex.RotationUnits.DEG)
-            print_brain('Autonomous Routine [OKAY]')
-
-        elif Positions.RED_LEFT == field_position:
-            print_brain('Autonomous Routine [OKAY]')
-
-        elif None == field_position:
-            print_brain('Autonomous Routine [FAILED]: None Variable Value')
-        else:
-            print_brain('Autonomous Routine [FAILED]: Unknown Error')
-
-        drivercontrol = True
-    Autonomous()
     print_brain('Driver Control [RUNNING]')
-    print_brain('Driver Control [OKAY]')
+    if drivercontrol != True:
+        print_brain('Driver Control [OKAY]')
     while drivercontrol:
         left_stick_y = controller.axis3.position()
-        right_stick_x = controller.axis2.position()
+        right_stick_x = controller.axis1.position()
 
-        if left_stick_y > 0 or left_stick_y < 0:
-            drivetrain.drive(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
-        elif right_stick_x > 0:
-            drivetrain.turn(vex.TurnType.RIGHT, right_stick_x, vex.VelocityUnits.PERCENT)
-        elif right_stick_x < 0:
-            drivetrain.turn(vex.TurnType.LEFT, right_stick_x, vex.VelocityUnits.PERCENT)
+        if left_stick_y != 0:
+            leftmotor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
+            rightmotor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
+        elif right_stick_x != 0:
+            leftmotor.spin(vex.DirectionType.FORWARD, right_stick_x, vex.VelocityUnits.PERCENT)
+            rightmotor.spin(vex.DirectionType.FORWARD, (right_stick_x * -1), vex.VelocityUnits.PERCENT)        
         else:
-            drivetrain.stop()
-
+            leftmotor.stop()
+            rightmotor.stop()
 Main()
