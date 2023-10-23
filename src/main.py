8b0e62154c71dc85422e96f6bbb6a26b8ed7aa9a
vx-clutch <owestness@gmail.com>
2023-10-23 19:53:42 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -3,16 +3,26 @@ from vex import *
 
 brain = vex.Brain()
 controller = vex.Controller()
-leftmotor = vex.Motor(vex.Ports.PORT1, vex.GearSetting.RATIO_18_1, False)
-rightmotor = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
-drivetrain = DriveTrain(leftmotor, rightmotor)
+left_drive_motor = vex.Motor(vex.Ports.PORT1, vex.GearSetting.RATIO_18_1, False)
+right_drive_motor = vex.Motor(vex.Ports.PORT10, vex.GearSetting.RATIO_18_1, True)
+arm_swing_motor = None
+drivetrain = DriveTrain(left_drive_motor, right_drive_motor)
 
-drivercontrol = False
+driver_control = False
 field_position = -1
 
 def print_brain(msg):
     brain.screen.print(msg)
     brain.screen.new_line()
+
+def launchElement():
+    pass
+
+def handelElementUp():
+    pass
+
+def handelElementDown():
+    pass
 
 def Main():
     print_brain('Initialized [RUNNING]')
@@ -42,33 +52,33 @@ def Main():
             print_brain('Autonomous Routine [OKAY]')
 
         elif None == field_position:
-            print_brain('Autonomous Routine [FAILED]: None Variable Value')
+            print_brain('Autonomous Routine [FAILED]: Error None Variable Value')
         else:
             print_brain('Autonomous Routine [FAILED]: Unknown Error')
 
-        drivercontrol = True
+        driver_control = True
 
     Autonomous()
 
     print_brain('Driver Control [RUNNING]')
-    if drivercontrol == False:
-        print_brain(f'Driver Control [FAILED]: Driver Control is set to {drivercontrol}')
-    elif drivercontrol == True:
+    if bool(driver_control):
+        print_brain(f'Driver Control [FAILED]: Error {driver_control} Variable Value')
+    elif driver_control:
         print_brain('Driver Control [OKAY]')
     else:
-        print_brain(f'Driver Control [FAILED]: Driver Control is set to {drivercontrol}')
-    while drivercontrol:
+        print_brain(f'Driver Control [FAILED]: Unknown Error')
+    while driver_control:
         left_stick_y = controller.axis3.position()
         right_stick_x = controller.axis1.position()
 
         if left_stick_y != 0:
-            leftmotor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
-            rightmotor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
+            left_drive_motor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
+            right_drive_motor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
         elif right_stick_x != 0:
-            leftmotor.spin(vex.DirectionType.FORWARD, right_stick_x, vex.VelocityUnits.PERCENT)
-            rightmotor.spin(vex.DirectionType.BACKWARD, right_stick_x, vex.VelocityUnits.PERCENT)        
+            left_drive_motor.spin(vex.DirectionType.FORWARD, right_stick_x, vex.VelocityUnits.PERCENT)
+            right_drive_motor.spin(vex.DirectionType.REVERSE, right_stick_x, vex.VelocityUnits.PERCENT)        
         else:
-            leftmotor.stop()
-            rightmotor.stop()
+            left_drive_motor.stop()
+            right_drive_motor.stop()
 
 Main()
