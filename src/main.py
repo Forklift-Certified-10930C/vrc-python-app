c4b7be1a81988d5535fb35d4f025ab2d6712ba3e
vx-clutch <owestness@gmail.com>
2023-10-25 21:25:48 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ arm_swing_motor = vex.Motor(vex.Ports.PORT11, vex.GearSetting.RATIO_18_1, False)
 drivetrain = DriveTrain(left_drive_motor, right_drive_motor)
 
 driver_control = False
-field_position = -1
+field_position = None
 
 def print_brain(msg):
     brain.screen.print(msg)
@@ -42,6 +42,8 @@ def Main():
             print_brain('Autonomous Routine [OKAY]')
 
         elif Positions.BLUE_LEFT == field_position:
+            drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
+            drivetrain.turn_for(vex.TurnType.LEFT, 90, vex.RotationUnits.DEG)
             print_brain('Autonomous Routine [OKAY]')
 
         elif Positions.RED_RIGHT == field_position:
@@ -57,29 +59,39 @@ def Main():
         else:
             print_brain('Autonomous Routine [FAILED]: Unknown Error')
 
-        driver_control = True
-
     Autonomous()
 
     print_brain('Driver Control [RUNNING]')
+    driver_control = True
     if driver_control == False:
-        print_brain(f'Driver Control [FAILED]: Error {driver_control} Variable Value')
+        print_brain('Driver Control [FAILED]: Error Driver Control False')
     elif driver_control:
         print_brain('Driver Control [OKAY]')
     else:
-        print_brain(f'Driver Control [FAILED]: Unknown Error')
+        print_brain('Driver Control [FAILED]: Unknown Error')
     while driver_control:
         left_stick_y = controller.axis3.position()
         right_stick_x = controller.axis1.position()
+        up = controller.buttonUp.pressing()
+        down = controller.buttonDown.pressing()
+        left = controller.buttonLeft.pressing()
+        right = controller.buttonRight.pressing()
 
         if left_stick_y != 0:
             left_drive_motor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
             right_drive_motor.spin(vex.DirectionType.FORWARD, left_stick_y, vex.VelocityUnits.PERCENT)
         elif right_stick_x != 0:
             left_drive_motor.spin(vex.DirectionType.FORWARD, right_stick_x, vex.VelocityUnits.PERCENT)
-            right_drive_motor.spin(vex.DirectionType.REVERSE, right_stick_x, vex.VelocityUnits.PERCENT)        
+            right_drive_motor.spin(vex.DirectionType.REVERSE, right_stick_x, vex.VelocityUnits.PERCENT)
         else:
             left_drive_motor.stop()
             right_drive_motor.stop()
+
+# while field_position == None:
+#     up = controller.buttonUp.pressing()
+#     down = controller.buttonDown.pressing()
+#     left = controller.buttonLeft.pressing()
+#     right = controller.buttonRight.pressing()
+
 
 Main()
