931bcf112e21e5ed1e3e7536259a2ed34286699c
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-11-02 14:26:11 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def print_brain(typeIn, process, msg='',):
         type = '[FAILED]'
     if typeIN.lower() == okay:
         type = '[OKAY]'
-    brain.screen.print(STR(process.lower()).capitalize() + f' {type} ' + msg)
+    brain.screen.print(STR(process.lower()).capitalize() + f' {type}: ' + msg.upper())
     brain.screen.new_line()
 
 def launchElement():
@@ -55,34 +55,34 @@ def Main():
         elif Positions.BLUE_LEFT == field_position:
             drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
             drivetrain.turn_for(vex.TurnType.LEFT, 90, vex.RotationUnits.DEG)
-            print_brain('Autonomous Routine [OKAY]: 2')
+            print_brain('okay', 'Autonomous Routine', '2')
 
         elif Positions.RED_RIGHT == field_position:
             drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
             drivetrain.turn_for(vex.TurnType.LEFT, 90, vex.RotationUnits.DEG)
-            print_brain('Autonomous Routine [OKAY]: -1')
+            print_brain('okay', 'Autonomous Routine', '-1')
 
         elif Positions.RED_LEFT == field_position:
             drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
             drivetrain.turn_for(vex.TurnType.LEFT, 90, vex.RotationUnits.DEG)
             launchElement()
-            print_brain('-2','run')
+            print_brain('okay', 'Autonomous Routine', '-2')
 
         elif None == field_position:
-            print_brain('Autonomous Routine [FAILED]: Error None Variable Value')
+            print_brain('fail', 'Autonomous Routine', 'ERR_None')
         else:
-            print_brain('Autonomous Routine [FAILED]: Unknown Error')
+            print_brain('fail', 'Autonomous Routine', 'ERR_UNKOWN')
 
     Autonomous()
 
-    print_brain('Driver Control [RUNNING]')
+    print_brain('run', 'driver control')
     driver_control = True
     if driver_control == False:
-        print_brain('Driver Control [FAILED]: Error Driver Control False')
+        print_brain('fail', 'driver control', 'ERR_FALSE')
     elif driver_control:
-        print_brain('Driver Control [OKAY]')
+        print_brain('okay', 'driver control')
     else:
-        print_brain('Driver Control [FAILED]: Unknown Error')
+        print_brain('fail', 'driver control', 'ERR_UNKOWN')
     while driver_control:
         left_stick_y = controller.axis3.position()
         right_stick_x = controller.axis1.position()
