8cf856497aeae4f0eb7f14df542f0ef26eae2b31
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-11-02 07:31:59 -0400

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -11,8 +11,15 @@ drivetrain = DriveTrain(left_drive_motor, right_drive_motor)
 driver_control = False
 field_position = None
 
-def print_brain(msg):
-    brain.screen.print(msg)
+def print_brain(process, msg='', typeIn):
+    type = ''
+    if typeIn.lower() == run:
+        type = '[RUNNING]'
+    if typeIn.lower() == fail:
+        type = '[FAILED]'
+    if typeIN.lower() == okay:
+        type = '[OKAY]'
+    brain.screen.print(STR(process.lower()).capitalize() + f' {type} ' + msg)
     brain.screen.new_line()
 
 def launchElement():
@@ -27,7 +34,7 @@ def handelElementDown():
     pass
 
 def Main():
-    print_brain('Initialized [RUNNING]')
+    print_brain('Initialized',run,)
     print_brain('Initialized [OKAY]')
 
     def Autonomous():
@@ -59,7 +66,7 @@ def Main():
             drivetrain.drive_for(vex.DirectionType.FORWARD, 1066, vex.DistanceUnits.MM)
             drivetrain.turn_for(vex.TurnType.LEFT, 90, vex.RotationUnits.DEG)
             launchElement()
-            print_brain('Autonomous Routine [OKAY]: -2')
+            print_brain('-2','run')
 
         elif None == field_position:
             print_brain('Autonomous Routine [FAILED]: Error None Variable Value')
