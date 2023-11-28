63e79e46318b7a55c7f84fc1eed742c3dd9093b9
vx-clutch <owestness@gmail.com>
2023-11-27 20:51:12 -0500

    Updates to main.py

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ selectedPosition=None
 hasObject=True
 deadZone=10
 inMotion=False
+isSkill=False
 def printToBrain(err, func):
     brain.screen.print("[ {} ] Error {}: at <{}>".format(brain.timer.time(TimeUnits.MSEC), err, func))
     brain.screen.new_line()
@@ -18,35 +19,37 @@ def throwObject():
 def handleObjectDown():
     return 0, 'handleObjectDown'
 def teamChoosing():
+    global isSkill
     while True:
         if controller.buttonL1.pressing():
             brain.screen.draw_image_from_file("red_offence.png", 0, 0)
             while controller.buttonL1.pressing():
                 wait(5, MSEC)
             brain.screen.clear_screen()
-            return 0, 'teamChoosing', "red_offence"
+            return 0, 'teamChoosing', "Red Offence"
         if controller.buttonL2.pressing():
             brain.screen.draw_image_from_file("red_defence.png", 0, 0)
             while controller.buttonL2.pressing():
                 wait(5, MSEC)
             brain.screen.clear_screen()
-            return 0, 'teamChoosing', "red_defence"
+            return 0, 'teamChoosing', "Red Defence"
         if controller.buttonR1.pressing():
             brain.screen.draw_image_from_file("blue_offence.png", 0, 0)
             while controller.buttonR1.pressing():
                 wait(5, MSEC)
             brain.screen.clear_screen()
-            return 0, 'teamChoosing', "blue_offence"
+            return 0, 'teamChoosing', "Blue Offence"
         if controller.buttonR2.pressing():
             brain.screen.draw_image_from_file("blue_defence.png", 0, 0)
             while controller.buttonR2.pressing():
                 wait(5, MSEC)
             brain.screen.clear_screen()
-            return 0, 'teamChoosing', "blue_defence"
+            return 0, 'teamChoosing', "Blue Defence"
         if controller.buttonA.pressing():
             brain.screen.draw_image_from_file("skill_confirmed.png", 0, 0)
             while controller.buttonA.pressing():
                 wait(5, MSEC)
+            isSkill=True
             brain.screen.clear_screen()
         wait(20)
 def autonomous():
@@ -66,7 +69,7 @@ def autonomous():
         printToBrain(result, functionName)
         return 0, 'autonomous'
 def driverControl():
-    global selectedPosition, hasObject, deadZone, inMotion
+    global selectedPosition, hasObject, deadZone, inMotion, isSkill
     while True:
         if controller.buttonUp.pressing():
             drivetrain.set_stopping(COAST)
@@ -90,6 +93,7 @@ def driverControl():
             inMotion=False
         wait(20)
 result, functionName, position=teamChoosing()
-printToBrain(result, functionName)
 selectedPosition=position
-result, functionName, selectedPosition=teamChoosing()
+printToBrain(result, functionName)
+autonomous()
+driverControl()
