9e3a8fc80965e15022e0810a7c3851e4d6761c94
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-11-30 18:43:47 -0500

    Update main.py
    Signed-off-by: vx_clutch <98831688+vx-clutch@users.noreply.github.com>

---
+++ src/main.py
@@ -0,0 +1,41 @@
+myVariable = 0
+
+def when_started1():
+    global myVariable
+    pass
+
+def ondriver_drivercontrol_0():
+    global myVariable
+    pass
+
+def onauton_autonomous_0():
+    global myVariable
+    pass
+
+# create a function for handling the starting and stopping of all autonomous tasks
+def vexcode_auton_function():
+    # Start the autonomous control tasks
+    auton_task_0 = Thread( onauton_autonomous_0 )
+    # wait for the driver control period to end
+    while( competition.is_autonomous() and competition.is_enabled() ):
+        # wait 10 milliseconds before checking again
+        wait( 10, MSEC )
+    # Stop the autonomous control tasks
+    auton_task_0.stop()
+
+def vexcode_driver_function():
+    # Start the driver control tasks
+    driver_control_task_0 = Thread( ondriver_drivercontrol_0 )
+
+    # wait for the driver control period to end
+    while( competition.is_driver_control() and competition.is_enabled() ):
+        # wait 10 milliseconds before checking again
+        wait( 10, MSEC )
+    # Stop the driver control tasks
+    driver_control_task_0.stop()
+
+
+# register the competition functions
+competition = Competition( vexcode_driver_function, vexcode_auton_function )
+
+when_started1()
