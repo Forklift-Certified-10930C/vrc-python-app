65622cfef31c10ed1ea51115b376aef3f266cc43
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-12-09 21:01:20 -0500

    save

---
+++ skills/driver/driver.py
@@ -0,0 +1,15 @@
+from vex import *
+
+brain=Brain()
+controller=Controller(PRIMARY)
+motor_l=Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+motor_r=Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
+motor_group_l=MotorGroup(motor_l)
+motor_group_r=MotorGroup(motor_r)
+drivetrain=DriveTrain(motor_group_l, motor_group_r)
+motor_top=Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)
+motor_bottom=Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)
+motor_group_throw=MotorGroup(motor_top, motor_bottom)
+motor_front_left=Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
+motor_front_right=Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
+motor_group_intake=MotorGroup(motor_front_left, motor_front_right)
