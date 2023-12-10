1b54b0554e6a6086f5f0f420823eb566a39e4deb
vx_clutch <98831688+vx-clutch@users.noreply.github.com>
2023-12-10 09:31:39 -0500

    save

--- skills/src/programming/programming.py
+++ skills/src/programming/programming.py
@@ -13,3 +13,18 @@ motor_group_throw=MotorGroup(motor_top, motor_bottom)
 motor_front_left=Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
 motor_front_right=Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
 motor_group_intake=MotorGroup(motor_front_left, motor_front_right)
+
+def main():
+    pass
+
+def vexcode_coding_skills():
+    skill_task_0 = Thread( main )
+    while(competition.is_autonomous() and competition.is_enabled() ):
+        wait(10,MSEC)
+    skill_task_0.stop()
+
+def vexcode_placeholder_function():
+    pass
+
+# register the competition functions
+competition = Competition( vexcode_placeholder_function, vexcode_coding_skills )
