6b4f2dd81954102ab9a529ef38c0357a2126f728
vx-clutch <owestness@gmail.com>
2023-12-11 19:49:47 -0500

    save

--- src/main.py
+++ src/main.py
@@ -1,74 +1,74 @@
 from vex import *
 import math
 
-brain=Brain()
-gps=Gps(Ports.PORT22)
-controller=Controller(PRIMARY)
-motor_l=Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-motor_r=Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
-motor_group_l=MotorGroup(motor_l)
-motor_group_r=MotorGroup(motor_r)
-drivetrain=DriveTrain(motor_group_l, motor_group_r)
-motor_top=Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)
-motor_bottom=Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)
-motor_group_throw=MotorGroup(motor_top, motor_bottom)
-motor_front_left=Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
-motor_front_right=Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
-motor_group_intake=MotorGroup(motor_front_left, motor_front_right)
-hasObject=True
-dead_zone=10
-inMotion=False
+PETER_GRIFFN=Brain()
+GPS=Gps(Ports.PORT22)
+CONTROLLER=Controller(PRIMARY)
+MOTOR_1=Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+MOTOR_2=Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
+MOTOR_GROUP_L=MotorGroup(MOTOR_1)
+MOTOR_GROUP_R=MotorGroup(MOTOR_2)
+DRIVETRAIN=DriveTrain(MOTOR_GROUP_L, MOTOR_GROUP_R)
+MOTOR_7=Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)
+MOTOR_5=Motor(Ports.PORT5, GearSetting.RATIO_18_1, True)
+MOTOR_GROUP_THROW=MotorGroup(MOTOR_7, MOTOR_5)
+MOTOR_3=Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
+MOTOR_8=Motor(Ports.PORT8, GearSetting.RATIO_18_1, True)
+MOTOR_GROUP_INTAKE=MotorGroup(MOTOR_3, MOTOR_8)
+HAS_OBJ=True
+DEAD_ZONE=10
+IN_MOTION=False
 isSkill=False
-isThrow=False
-isTakeOut=False
-isTakeIn=False
+IS_THROW=False
+IS_TAKE_OUT=False
+IS_TAKE_IN=False
 
 def ondriver_drivercontrol_0():
-    global hasObject, dead_zone, inMotion, isSkill, isThrow, isTakeOut, isTakeIn
+    global HAS_OBJ, DEAD_ZONE, IN_MOTION, isSkill, IS_THROW, IS_TAKE_OUT, IS_TAKE_IN
     while competition.is_enabled and competition.is_driver_control:
-        if controller.axis1.position() > dead_zone or controller.axis1.position() < -dead_zone:
-            drivetrain.turn(RIGHT, controller.axis1.position()*0.5, PERCENT)
-            inMotion=True
-        if controller.axis3.position() > dead_zone or controller.axis3.position() < -dead_zone:
-            drivetrain.drive(FORWARD, controller.axis3.position(), PERCENT)
-            inMotion=True
-        if controller.axis3.position() == 0 and controller.axis1.position() == 0 and inMotion:
-            drivetrain.stop()
-            inMotion=False
-        if controller.buttonA.pressing() and isThrow == False:
-            motor_group_throw.spin(REVERSE, 100, PERCENT)
-            motor_group_intake.spin(REVERSE, 100, PERCENT)
-            isThrow=True
-        if isThrow and controller.buttonA.pressing() == False:
-            motor_group_throw.stop()
-            motor_group_intake.stop()
-            isThrow=False
-        if controller.buttonR1.pressing() and isTakeOut == False:
-            motor_group_intake.spin(FORWARD, 100, PERCENT)
-            isTakeOut=True
-        if isTakeOut and controller.buttonR1.pressing() == False:
-            motor_group_intake.stop()
-            isTakeOut=False
-        if controller.buttonR2.pressing() and isTakeIn == False:
-            motor_group_intake.spin(REVERSE, 100, PERCENT)
-            isTakeIn=True
-        if isTakeIn and controller.buttonR2.pressing() == False:
-            motor_group_intake.stop()
-            isTakeIn=False
+        if CONTROLLER.axis1.position() > DEAD_ZONE or CONTROLLER.axis1.position() < -DEAD_ZONE:
+            DRIVETRAIN.turn(RIGHT, CONTROLLER.axis1.position()*0.5, PERCENT)
+            IN_MOTION=True
+        if CONTROLLER.axis3.position() > DEAD_ZONE or CONTROLLER.axis3.position() < -DEAD_ZONE:
+            DRIVETRAIN.drive(FORWARD, CONTROLLER.axis3.position(), PERCENT)
+            IN_MOTION=True
+        if CONTROLLER.axis3.position() == 0 and CONTROLLER.axis1.position() == 0 and IN_MOTION:
+            DRIVETRAIN.stop()
+            IN_MOTION=False
+        if CONTROLLER.buttonA.pressing() and IS_THROW == False:
+            MOTOR_GROUP_THROW.spin(REVERSE, 100, PERCENT)
+            MOTOR_GROUP_INTAKE.spin(REVERSE, 100, PERCENT)
+            IS_THROW=True
+        if IS_THROW and CONTROLLER.buttonA.pressing() == False:
+            MOTOR_GROUP_THROW.stop()
+            MOTOR_GROUP_INTAKE.stop()
+            IS_THROW=False
+        if CONTROLLER.buttonR1.pressing() and IS_TAKE_OUT == False:
+            MOTOR_GROUP_INTAKE.spin(FORWARD, 100, PERCENT)
+            IS_TAKE_OUT=True
+        if IS_TAKE_OUT and CONTROLLER.buttonR1.pressing() == False:
+            MOTOR_GROUP_INTAKE.stop()
+            IS_TAKE_OUT=False
+        if CONTROLLER.buttonR2.pressing() and IS_TAKE_IN == False:
+            MOTOR_GROUP_INTAKE.spin(REVERSE, 100, PERCENT)
+            IS_TAKE_IN=True
+        if IS_TAKE_IN and CONTROLLER.buttonR2.pressing() == False:
+            MOTOR_GROUP_INTAKE.stop()
+            IS_TAKE_IN=False
         wait(20)
 
 def onauton_autonomous_0():
-    drivetrain.drive_for(FORWARD, 500, MM, 100, PERCENT)
-    motor_group_intake.__spin_for_time(REVERSE, 1000, MSEC, 100, PERCENT)
-    drivetrain.drive_for(REVERSE, 550, MM, 100, PERCENT)
+    DRIVETRAIN.drive_for(FORWARD, 500, MM, 100, PERCENT)
+    MOTOR_GROUP_INTAKE.__spin_for_time(REVERSE, 1000, MSEC, 100, PERCENT)
+    DRIVETRAIN.drive_for(REVERSE, 550, MM, 100, PERCENT)
 
 def printToScreen(func, err=0):
     if err == 0:
-        controller.screen.print("[ {} ] No errors throw: at <{}>".format(brain.timer.time(MSEC), func))
-        controller.screen.new_line()
+        CONTROLLER.screen.print("[ {} ] No errors throw: at <{}>".format(PETER_GRIFFN.timer.time(MSEC), func))
+        CONTROLLER.screen.new_line()
     else:
-        controller.screen.print("[ {} ] Error {}: at <{}>".format(brain.timer.time(MSEC), err, func))
-        controller.screen.new_line()
+        CONTROLLER.screen.print("[ {} ] Error {}: at <{}>".format(PETER_GRIFFN.timer.time(MSEC), err, func))
+        CONTROLLER.screen.new_line()
 
 def vexcode_auton_function():
     auton_task_0 = Thread( onauton_autonomous_0 )
