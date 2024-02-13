448502829e4c94e97c890acf1094fe3ca9773720
vx_clutch <owestness@gmail.com>
2024-02-13 16:26:03 -0500

    feat: Made the laucnher default to the low position; minor: renamed things

--- src/main.py
+++ src/main.py
@@ -11,10 +11,9 @@ MOTOR_17=Motor(Ports.PORT17, GearSetting.RATIO_36_1, False)
 MOTOR_19=Motor(Ports.PORT19, GearSetting.RATIO_6_1, True)
 MOTOR_20=Motor(Ports.PORT20, GearSetting.RATIO_18_1, False)
 
-
 MOTOR_GROUP_L=MotorGroup(MOTOR_20)
 MOTOR_GROUP_R=MotorGroup(MOTOR_10)
-MOTOR_GROUP_THROW=MotorGroup(MOTOR_7,MOTOR_17)
+MOTOR_GROUP_LAUNCHER=MotorGroup(MOTOR_7,MOTOR_17)
 MOTOR_GROUP_INTAKE=MotorGroup(MOTOR_9, MOTOR_19)
 MOTOR_GROUP_WINGS=MotorGroup(MOTOR_11)
 
@@ -22,11 +21,11 @@ DRIVETRAIN=DriveTrain(MOTOR_GROUP_L, MOTOR_GROUP_R)
 
 # Settings
 DEAD_ZONE_PERCENT=10
-THROW_VEL_PERCENT=30
+LAUNCHER_VEL_PERCENT=30
 INTAKE_SPEED_PERCENT=100
 TURN_DAMP_PERCENT=0.5
-ARM_SPEED_PERCENT=25
-ARM_FLACID_POSITION_DEGREES=135
+LAUNCHER_SPEED_PERCENT=25
+LAUNCHER_DEFAULT_POSITION_DEGREES=135
 
 AUTONOMOUS_SPEED_PERCENT=100
 AUTONOMOUS_INTAKE_SPEED_PERCENT=100
@@ -36,15 +35,15 @@ AUTONOMOUS_DRIVE_DISTANCE_MM_MARGIN=50
 
 IN_MOTION=False
 IS_SKILL=False
-IS_THROW=False
+IS_LAUNCHER=False
 IS_TAKE_OUT=False
 IS_TAKE_IN=False
 WING_STATUS=False
 START_ANGLE_7=0
 START_ANGLE_11=0
 
-def ondriver_drivercontrol_0():
-    global DEAD_ZONE_PERCENT, IN_MOTION, IS_SKILL, IS_THROW, IS_TAKE_OUT, IS_TAKE_IN, WING_STATUS
+def ondriver_drivercontrol():
+    global DEAD_ZONE_PERCENT, IN_MOTION, IS_SKILL, IS_LAUNCHER, IS_TAKE_OUT, IS_TAKE_IN, WING_STATUS
     while competition.is_enabled and competition.is_driver_control:
         if CONTROLLER.axis1.position() > DEAD_ZONE_PERCENT or CONTROLLER.axis1.position() < -DEAD_ZONE_PERCENT:
             DRIVETRAIN.turn(RIGHT, CONTROLLER.axis1.position()*TURN_DAMP_PERCENT, PERCENT)
@@ -57,20 +56,20 @@ def ondriver_drivercontrol_0():
             DRIVETRAIN.stop()
             IN_MOTION=False
 
-        if CONTROLLER.buttonA.pressing() and IS_THROW == False:
-            MOTOR_GROUP_THROW.spin(FORWARD, THROW_VEL_PERCENT, PERCENT)
-            IS_THROW=True
-        if IS_THROW and CONTROLLER.buttonA.pressing() == False:
-            MOTOR_GROUP_THROW.stop(COAST)
-            IS_THROW=False
+        if CONTROLLER.buttonA.pressing() and IS_LAUNCHER == False:
+            MOTOR_GROUP_LAUNCHER.spin(FORWARD, THROW_VEL_PERCENT, PERCENT)
+            IS_LAUNCHER=True
+        if IS_LAUNCHER and CONTROLLER.buttonA.pressing() == False:
+            MOTOR_GROUP_LAUNCHER.stop(COAST)
+            IS_LAUNCHER=False
         
         if WING_STATUS == False and CONTROLLER.buttonX.pressing():
-            arms(True)
+            wings(True)
             WING_STATUS=True
             while CONTROLLER.buttonX.pressing():
                 wait(10, MSEC)
         if WING_STATUS and CONTROLLER.buttonX.pressing():
-            arms(False)
+            wings(False)
             WING_STATUS=False
         
         if CONTROLLER.buttonR1.pressing() and IS_TAKE_OUT == False:
@@ -78,6 +77,7 @@ def ondriver_drivercontrol_0():
             IS_TAKE_OUT=True
         if IS_TAKE_OUT and CONTROLLER.buttonR1.pressing() == False:
             MOTOR_GROUP_INTAKE.stop()
+            MOTOR_GROUP_LAUNCHER.set_position(LAUNCHER_DEFAULT_POSITION_DEGREES, DEGREES)
             IS_TAKE_OUT=False
 
         if CONTROLLER.buttonR2.pressing() and IS_TAKE_IN == False:
@@ -87,12 +87,11 @@ def ondriver_drivercontrol_0():
             MOTOR_GROUP_INTAKE.stop()
             IS_TAKE_IN=False
 
-        if CONTROLLER.buttonUp.pressing():
-            MOTOR_GROUP_THROW.set_position(ARM_FLACID_POSITION_DEGREES, DEGREES)
-
+        # if CONTROLLER.buttonUp.pressing():
+            # MOTOR_GROUP_LAUNCHER.set_position(LAUNCHER_DEFAULT_POSITION_DEGREES, DEGREES)
         wait(20)
 
-def onauton_autonomous_0():
+def onauton_autonomous():
     DRIVETRAIN.drive_for(FORWARD, AUTONOMOUS_DRIVE_DISTANCE_MM, MM, AUTONOMOUS_SPEED_PERCENT, PERCENT)
     MOTOR_GROUP_INTAKE.__spin_for_time(FORWARD, AUTONOMOUS_INTAKE_TIME_MSEC, MSEC, AUTONOMOUS_INTAKE_SPEED_PERCENT, PERCENT)
     DRIVETRAIN.drive_for(REVERSE, (AUTONOMOUS_DRIVE_DISTANCE_MM + AUTONOMOUS_DRIVE_DISTANCE_MM_MARGIN), MM, AUTONOMOUS_SPEED_PERCENT, PERCENT)
@@ -110,26 +109,26 @@ def printMsgToScreen(msg, func):
     BRIAN.screen.new_line()
 
 def vexcode_auton_function():
-    auton_task_0 = Thread( onauton_autonomous_0 )
+    auton_task = Thread( onauton_autonomous_0 )
     while( competition.is_autonomous() and competition.is_enabled() ):
         wait( 10, MSEC )
-    auton_task_0.stop()
+    auton_task.stop()
 
 def vexcode_driver_function():
-    driver_control_task_0 = Thread( ondriver_drivercontrol_0 )
+    driver_control_task = Thread( ondriver_drivercontrol_0 )
 
     while( competition.is_driver_control() and competition.is_enabled() ):
         wait( 10, MSEC )
-    driver_control_task_0.stop()
+    driver_control_task.stop()
 
-def arms(pos):
+def wings(pos):
     global START_ANGLE_7, START_ANGLE_11
     if pos:
-        MOTOR_11.spin_to_position(85, DEGREES, ARM_SPEED_PERCENT, PERCENT)
-        MOTOR_7.spin_to_position(40, DEGREES, ARM_SPEED_PERCENT, PERCENT)
+        MOTOR_11.spin_to_position(85, DEGREES, LAUNCHER_SPEED_PERCENT, PERCENT)
+        MOTOR_7.spin_to_position(40, DEGREES, LAUNCHER_SPEED_PERCENT, PERCENT)
     else:
-        MOTOR_11.spin_to_position(START_ANGLE_7, DEGREES, ARM_SPEED_PERCENT, PERCENT)
-        MOTOR_7.spin_to_position(START_ANGLE_11, DEGREES, ARM_SPEED_PERCENT, PERCENT)
+        MOTOR_11.spin_to_position(START_ANGLE_7, DEGREES, LAUNCHER_SPEED_PERCENT, PERCENT)
+        MOTOR_7.spin_to_position(START_ANGLE_11, DEGREES, LAUNCHER_SPEED_PERCENT, PERCENT)
 
 def calibrate():
     global START_ANGLE_7, START_ANGLE_11
@@ -138,3 +137,4 @@ def calibrate():
 calibrate()
 
 competition = Competition( vexcode_driver_function, vexcode_auton_function )
+
